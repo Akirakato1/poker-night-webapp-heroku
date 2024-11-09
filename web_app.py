d7e7386142000385868e014811cb0fc33e7ce224
Akirakato1 <32103237+Akirakato1@users.noreply.github.com>
2024-11-09 01:23:49 -0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -2,11 +2,8 @@
 from flask import Flask, jsonify
 from DBManager import DBManager
 
-db=DBManager()
 app = Flask(__name__)
-
-# Use the shared connection
-db_connection = init_connection()
+db=DBManager()
 
 @app.route("/")
 def inspect_data():
